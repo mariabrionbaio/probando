bf60af0d6d9d4fdfda4303a3985a042c3b6ee8dc
mbdorca <mbdorca@DPTO-62.iesbaio.zas>
2026-01-20 09:58:18 +0100

    primeira subida

---
+++ src/main/java/probando/Probando.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package probando;
+
+/**
+ *
+ * @author mbdorca
+ */
+public class Probando {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
