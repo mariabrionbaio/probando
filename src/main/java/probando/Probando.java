35acc085f67ea4b8edebf470513d69738f796623
mbdorca <mariabrion.baio@gmail.com>
2026-01-20 10:02:29 +0100

    primeira subida

--- src/main/java/probando/Probando.java
+++ src/main/java/probando/Probando.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package probando;
 
-/**
- *
- * @author mbdorca
- */
 public class Probando {
 
     public static void main(String[] args) {
